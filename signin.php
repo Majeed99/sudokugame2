2a45ee898ded418258f4103b45fd2a57f7d3101f
Majeed99 <73822378+Majeed99@users.noreply.github.com>
2020-12-02 02:44:28 +0300

    Update signin.php

--- signin.php
+++ signin.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="signin_changePass_style.css">
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="fonts/fontawesome-free-5.13.0-web/css/all.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="all.css">
     <title>Sign In</title>
 </head>
 <body>
